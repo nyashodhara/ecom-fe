0466a2587b35e84b6fd9d2fe2295cf932a31bf56
Yashodhara <Yashodharagowdan@gmail.com>
2023-05-26 15:22:26 +0530

    failure

--- src/app/services/onboard.service.ts
+++ src/app/services/onboard.service.ts
@@ -10,32 +10,36 @@ import { BehaviorSubject } from 'rxjs';
 export class OnboardService {
   isLoggedIn = new BehaviorSubject<boolean>(false);
 
-  constructor(private http:HttpClient,private router:Router) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
-  userLogin(data:login){
-    return this.http.post("https://ecom-i7ht.onrender.com/customer/login",data,{observe:"response"}).subscribe((result)=>{
-      if(result){
-        this.isLoggedIn.next(true);
-        localStorage.setItem('auth',JSON.stringify(result.body));
-        this.router.navigate(['home']);
+  userLogin(data: login) {
+    return this.http.post("https://ecomproject-production.up.railway.app/customer/login", data, { observe: "response" }).subscribe((result:any) => {
+      if (result) {
+        if (result.body.status == 'Success') {
+          this.isLoggedIn.next(true);
+          localStorage.setItem('auth', JSON.stringify(result.body));
+          this.router.navigate(['home']);
+        }
       }
       console.log(result);
     });;
   }
 
-  userSignUp(data:signUp){
-    return this.http.post("https://ecom-i7ht.onrender.com/customer/register",data,{observe:"response"}).subscribe((result:any)=>{
-      if(result){
-        this.isLoggedIn.next(true);
-        localStorage.setItem('auth',JSON.stringify(result.body));
-        this.router.navigate(['home']);
+  userSignUp(data: signUp) {
+    return this.http.post("https://ecomproject-production.up.railway.app/customer/register", data, { observe: "response" }).subscribe((result: any) => {
+      if (result) {
+        if (result.body.status == 'Success') {
+          this.isLoggedIn.next(true);
+          localStorage.setItem('auth', JSON.stringify(result.body));
+          this.router.navigate(['home']);
+        }
       }
       console.log(result);
     });;
   }
 
-  reloadHome(){
-    if(localStorage.getItem('auth')){
+  reloadHome() {
+    if (localStorage.getItem('auth')) {
       this.isLoggedIn.next(true);
       this.router.navigate(['home']);
       console.log("service")
